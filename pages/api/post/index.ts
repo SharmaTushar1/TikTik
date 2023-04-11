8f40eb8c74035a0357dc0e0913c436001c789169
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-04-11 14:24:45 +0530

    Add sidebar

---
+++ pages/api/post/index.ts
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { allPostsQuery } from './../../../utils/queries';
+import { client } from './../../../utils/client';
+import type { NextApiRequest, NextApiResponse } from 'next' 
+import { json } from 'stream/consumers';
+
+export default async function handler(req: NextApiRequest,res: NextApiResponse) {
+  if (req.method === 'GET') {
+    const query = allPostsQuery();
+    const data = await client.fetch(query); // passing the query for fetching on the client
+    res.status(200).json(data);
+  }
+}
---
+++ utils/index.ts
@@ -0,0 +1,6 @@
+
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const fetchGoogleResponse = async (response: any, addUser: any) => {
+  
+};
