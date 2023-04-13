876b73019b767f20a9928e8bb5c25beee5a902ac
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-04-13 20:50:53 +0530

    Add upload functionality

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -10,5 +10,9 @@ export default async function handler(req: NextApiRequest,res: NextApiResponse) 
     // here's the API call from backend
     const data = await client.fetch(query); // passing the query for fetching on the client
     res.status(200).json(data);
+  } else if (req.method === 'POST') { // there's a post request to the database. Like upload video.
+    const document = req.body // get the document from the body.
+    client.create(document) // add that to backend
+    .then(() => res.status(201).json('Video Created'))
   }
 }
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -6,6 +6,8 @@ import axios from 'axios';
 import { SanityAssetDocument } from '@sanity/client';
 import useAuthStore from '../store/authStore';
 import { client } from '../utils/client';
+import { topics } from '../utils/constants';
+
 
 
 const Upload = () => {
@@ -13,13 +15,20 @@ const Upload = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [videoAsset, setVideoAsset] = useState<SanityAssetDocument | undefined>();
   const [wrongFileType, setWrongFileType] = useState(false);
+  const [caption, setCaption] = useState('');
+  const [category, setCategory] = useState(topics[0].name);
+  const [savingPost, setSavingPost] = useState(false);
+  const {userProfile} : {userProfile: any} = useAuthStore();
+  const router = useRouter();
 
   const uploadVideo = async (e: any) => {
     const selectedFile = e.target.files[0];
-    const fileTypes = ['video/mp4', 'video/webm', 'video/ogg'];
+    const fileTypes = ['video/mp4', 'video/webm', 'video/ogg']; //allowed formats
+
+    // check valid file type is uploaded or not
 
     if(fileTypes.includes(selectedFile.type)) {
-      client.assets.upload('file', selectedFile, {
+      client.assets.upload('file', selectedFile, { // upload to backend
         contentType: selectedFile.type,
         filename: selectedFile.name
       })
@@ -30,13 +39,41 @@ const Upload = () => {
     } else {
       setIsLoading(false);
       setWrongFileType(true);
-    }
+    } 
+  }
 
+
+  const handlePost = async () => {
+    if (caption && videoAsset?._id && category) {
+      setSavingPost(true);
+      // create a document with the data that we got from upload
+      const document = {
+        _type: 'post',
+        caption, 
+        video: {
+          _type: 'file',
+          asset: {
+            _type: 'reference', 
+            _ref: videoAsset._id
+          }
+        },
+        _userId: userProfile?._id, 
+        postedBy: {
+          _type: 'postedBy', 
+          _ref: userProfile?._id
+        }, 
+        topic: category
+      }
+
+      await axios.post('http://localhost:3000/api/post', document);
+      router.push('/');
+
+    }
   }
 
   return (
-    <div className='flex w-full h-full'>
-      <div className='bg-white rounded-lg'>
+    <div className='flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center'>
+      <div className='bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-between items-center p-14 pt-6'>
         <div>
           <div>
             <p className='text-2xl font-bold'>
@@ -51,45 +88,95 @@ const Upload = () => {
               isLoading ? (
                 <p>Uploading...</p>
               ): (
-                <div>
-                  {videoAsset ? (
-                    <div>
-                      <video 
-                        src={videoAsset.url}
-                        loop
-                        controls
-                        className='rounded=xl h[[450px] mt-16 bg-black'
-                      ></video>
-                    </div>
-                  ):(
-                    <label className='cursor-pointer'>
-                      <div className='flex flex-col items-center justify-center h-full'>
-                        <div className="flex flex-col items-center justify-center">
-                          <p className='font-bold text-xl'>
-                            <FaCloudUploadAlt
-                              className='text-gray-300 text-6xl'
-                            />
-                          </p>
-                          <p className='text-xl font-semibold'>Upload Video</p>
-                        </div>
-                        <p className='text-gray-400 text-center mt-10 text-sm leading-10'>
-                          MP4 or WebM or ogg <br />
-                          720x1280 or higher <br />
-                          Up to 10 minutes <br />
-                          Less than 2 GB
+              <div>
+                {videoAsset ? (
+                  <div>
+                    <video 
+                      src={videoAsset.url}
+                      loop
+                      controls
+                      className='rounded=xl h[[450px] mt-16 bg-black'
+                    ></video>
+                  </div>
+                ):(
+                  <label className='cursor-pointer'>
+                    <div className='flex flex-col items-center justify-center h-full'>
+                      <div className="flex flex-col items-center justify-center">
+                        <p className='font-bold text-xl'>
+                          <FaCloudUploadAlt
+                            className='text-gray-300 text-6xl'
+                          />
                         </p>
-                        <p className='bg-[#F51997] text-center mt-10 rounded text-white text-md font=medium p-2 w-52 outline-none'>Select File</p>
+                        <p className='text-xl font-semibold'>Upload Video</p>
                       </div>
-                      <input type="file"
-                        name = "upload-video"
-                        onChange = {uploadVideo}
-                        className='w-0 h-0'
-                      />
-                    </label>
+                      <p className='text-gray-400 text-center mt-10 text-sm leading-10'>
+                        MP4 or WebM or ogg <br />
+                        720x1280 or higher <br />
+                        Up to 10 minutes <br />
+                        Less than 2 GB
+                      </p>
+                      <p className='bg-[#F51997] text-center mt-10 rounded text-white text-md font=medium p-2 w-52 outline-none'>Select File</p>
+                    </div>
+                    <input type="file"
+                      name = "upload-video"
+                      onChange = {uploadVideo}
+                      className='w-0 h-0'
+                    />
+                  </label>
 
-                  )}
-                </div>
-              )}
+                )}
+              </div>
+            )}
+            {/* If some non video file is uploaded */}
+            {wrongFileType && (
+              <p className='text-center text-xl text-red-400 font-semibold mt-4 w[[250px]'>
+                Please select a video file
+              </p>
+            )}
+          </div>
+
+
+        </div>
+        {/* Form for taking the caption and topic for upload video */}
+        <div className='flex flex-col gap-3 pb-10'>
+          <label className='text-md font-medium'>Caption</label>
+          <input type="text" 
+            value = {caption}
+            onChange={(e) => setCaption(e.target.value)}
+            className='rounded outline-none text-md border-2 border-gray-200 p-2'
+          />
+          <label className='text-md font-medium'>
+              Choose a Category
+          </label>
+          <select onChange={(e) => {setCategory(e.target.value)}}
+            className='outline-none border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer'
+          >
+            {topics.map((topic) => (
+              <option
+                key = {topic.name}
+                className='outline-none capitalize bg-white text-gray-700 text-md p-2 hover:bg-slate-300'
+                value = {topic.name}
+              >
+                {topic.name}
+              </option>
+            ))}
+          </select>
+          {/* discard and upload button */}
+          <div className='flex gap-6 mt-10'>
+              <button
+                onClick={() => {}}
+                type = "button"
+                className='border-gray-300 border-2 text-md font-medium p-2 rounded w-28 lg:w-44 outline-none'
+              >
+                Discard
+              </button>
+              <button
+                onClick={handlePost}
+                type = "button"
+                className='bg-[#F51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none'
+              >
+                Post
+              </button>
           </div>
         </div>
 
